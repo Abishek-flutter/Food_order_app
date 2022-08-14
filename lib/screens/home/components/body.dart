2816c9304bfa072286c846cf1e559373246a11aa
Abishek-project <abishekabi992@gmail.com>
2022-08-14 21:41:33 +0530

    change

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,12 +1,10 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:food_order/constanst.dart';
-import 'package:food_order/screens/home/components/category.dart';
+
 import 'package:food_order/screens/home/components/category_list.dart';
 import 'package:food_order/screens/home/components/discountcard.dart';
-import 'package:food_order/screens/home/components/itemcard.dart';
+
 import 'package:food_order/screens/home/components/itemlist.dart';
 import 'package:food_order/screens/home/components/search_bar.dart';
 
